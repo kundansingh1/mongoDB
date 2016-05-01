dcfa33cc55e52ff6d0d4587d3634c0b5871152f4
Kundan <kundan.singh979+web@gmail.com>
2016-05-01 17:16:41 +0000

    stuck on 3rd challenge. But code is complete

--- find.js
+++ find.js
@@ -1,10 +1,18 @@
 var filterAge = parseInt(process.argv[2]);
-var mongo = require('mongodb').MongoClient
-            mongo.connect(url, function(err, db) {
-                var collection = db.collection('parrots');
-                collection.find({      
-                }).toArray(function(err, documents) {
-                    if(document)
-                    db.close();
-                })
-            })
+var mongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+
+mongo.connect(url, function(err, db) {
+    if (err) throw err;
+    var parrots = db.collection('parrots');
+    parrots.find({
+        age: {
+            $gt: +filterAge
+        }
+    }).toArray(function(err, documents) {
+        if(err) throw err
+        console.log(documents)
+        db.close
+    })
+});
